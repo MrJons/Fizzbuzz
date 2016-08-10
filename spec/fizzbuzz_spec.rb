a21dda5e61f4772fa0ea77bf8bf50186de7014b8
MrJons <shad_stick@hotmail.co.uk>
2016-08-10 19:03:07 +0100

    Full working fizzbuzz app developed with all tests passing.

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,19 @@
-require "./lib/fizzbuzz"
+require 'fizzbuzz'
 
 describe "fizzbuzz" do
-  it "returns 'fizz' when passed 3" do
-    expect(fizzbuzz(3).to eq "fizz")
+  it "returns 'fizz' when passed multiple of 3" do
+    expect(fizzbuzz(3)).to eq "fizz"
+  end
+
+  it "returns 'buzz' when passed multiple of 5" do
+    expect(fizzbuzz(5)).to eq "buzz"
+  end
+
+  it "returns 'fizzbuzz' when passed multiple of 15" do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  end
+
+  it "returns passed number when not multiple of 3, 5, or 15" do
+    expect(fizzbuzz(7)).to eq 7
   end
 end
